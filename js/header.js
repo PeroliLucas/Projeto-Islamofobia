1c5fe4cb33c59213720f549947d97b49942e1ad1
PeroliLucas <lucasoli.ifc@gmail.com>
2025-12-16 14:17:19 -0300

    header quase pronto

--- js/header.js
+++ js/header.js
@@ -5,37 +5,19 @@ fetch('/components/header.html')
     initHeader();
   });
 
-
 function initHeader() {
   const menuToggle = document.getElementById('menuToggle');
   const navMenu = document.getElementById('navMenu');
-  const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
 
   menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
-
-    if (!navMenu.classList.contains('active')) {
-      document
-        .querySelectorAll('.dropdown.active')
-        .forEach(d => d.classList.remove('active'));
-    }
   });
 
-  
-  dropdownToggles.forEach(toggle => {
-    toggle.addEventListener('click', e => {
+  document.querySelectorAll('.dropdown-toggle').forEach(link => {
+    link.addEventListener('click', e => {
       if (window.innerWidth <= 900) {
         e.preventDefault();
-
-        const parent = toggle.parentElement;
-
-        document
-          .querySelectorAll('.dropdown.active')
-          .forEach(d => {
-            if (d !== parent) d.classList.remove('active');
-          });
-
-        parent.classList.toggle('active');
+        link.parentElement.classList.toggle('active');
       }
     });
   });
