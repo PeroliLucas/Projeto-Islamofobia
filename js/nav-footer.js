00fcb4c193d316796aa64a771de19cc0b252d58f
PeroliLucas <lucasoli.ifc@gmail.com>
2025-12-17 15:49:15 -0300

    urrrrrr

--- js/nav-footer.js
+++ js/nav-footer.js
@@ -1,21 +1,19 @@
-fetch('../../components/header.html')
-  .then(res => {
-    if (!res.ok) throw new Error('Header não encontrado');
-    return res.text();
-  })
-  .then(html => {
-    document.body.insertAdjacentHTML('afterbegin', html);
-    initHeader();
-  })
-  .catch(err => console.error(err));
+document.addEventListener("DOMContentLoaded", () => {
+  const loadComponent = (path, position = 'end') => {
+    fetch(path)
+      .then(res => {
+        if (!res.ok) throw new Error(`${path} não encontrado`);
+        return res.text();
+      })
+      .then(html => {
+        if (position === 'start') document.body.insertAdjacentHTML('afterbegin', html);
+        else document.body.insertAdjacentHTML('beforeend', html);
 
-fetch('../../components/footer.html')
-  .then(res => {
-    if (!res.ok) throw new Error('Footer não encontrado');
-    return res.text();
-  })
-  .then(html => {
-    document.body.insertAdjacentHTML('beforeend', html);
-  })
-  .catch(err => console.error(err));
+        if (typeof initHeader === 'function') initHeader();
+      })
+      .catch(err => console.error(err));
+  };
 
+  loadComponent('/components/header.html', 'start');
+  loadComponent('/components/footer.html', 'end');
+});
