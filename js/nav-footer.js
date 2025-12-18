e8b4bbcd71a87606bf1fcc8e2822fe022a8b71f1
PeroliLucas <lucasoli.ifc@gmail.com>
2025-12-17 21:28:48 -0300

    LINKS PARA SERVER LOCAL

--- js/nav-footer.js
+++ js/nav-footer.js
@@ -1,9 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("../../components/header.html")
-    .then(res => res.text())
-    .then(html => document.getElementById("header").innerHTML = html);
+if (window.location.hostname.includes('github.io')) {
+  const base = document.createElement('base');
+  base.href = '/Projeto-Islamofobia/';
+  document.head.appendChild(base);
+}
 
-  fetch("../../components/footer.html")
-    .then(res => res.text())
-    .then(html => document.getElementById("footer").innerHTML = html);
-});
+function loadComponent(id, path) {
+  fetch(path)
+  .then(res => res.text())
+  .then(html => {
+    const element = document.getElementById(id);
+    element.innerHTML = html;
+  })
+  .catch(err => console.error(`Erro ao carregar ${path}`, err))
+}
+
+const basePath = window.location.hostname.includes('github.io') ? '/Projeto-Islamofobia/' : '/';
+
+loadComponent('header', basePath + 'components/header.html');
+loadComponent('footer', basePath + 'components/footer.html');
